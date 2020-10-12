9ae3ef8913017eec309e79d8e708d2374718aba0
Alain Orluk <alainwebdev@free.fr>
2020-10-12 08:13:22 +0200

    initialize

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
+/* eslint-disable */
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <MyNav />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyNav from "./components/MyNav.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    MyNav,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./assets/css/style.css"></style>
---
+++ src/components/MyNav.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="nav">
+        <div class="nav-container">
+        <div id="logo">
+            <img class="img-fluid" src="~@/assets/img/YouneedAlain.png" alt="logo" />
+        </div>
+        <h1>
+            <span id="afpa">AFPA</span>&nbsp;<span id="promo">DWWM20061</span>&nbsp;Messaging by <span id="me">&nbsp;Alain ORLUK</span>
+        </h1>
+        <!--<p id="dateJour"></p>
+            <h4>Date du jour</h4>-->
+        <ul id="btnsNav">
+            <li>
+            <i class="fas fa-user"></i>
+            </li>
+            <li>
+            <i class="fas fa-inbox"></i>
+            </li>
+            <li>
+            <i class="fas fa-sign-in-alt"></i>
+            </li>
+        </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyNav',
+};
+</script>
+
